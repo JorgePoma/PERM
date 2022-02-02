7850b570fffabed977d7167e31c8e4698392f1f4
JorgePB <jorge.poma@unl.edu.ec>
2022-02-02 09:06:30 -0500

    .env

--- MM_backend/src/db.js
+++ MM_backend/src/db.js
@@ -1,11 +1,12 @@
-const {Pool} = require('pg');
+const { Pool } = require('pg');
+const { db } = require('./config');
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'jorgepoma10',
-    host: 'localhost',
-    port: '5433',
-    database:'mm'
+    user: db.user,
+    password: db.password,
+    host: db.host,
+    port: db.port,
+    database: db.database
 });
 
 module.exports = pool;
